babe6cafe943ee8fabe7629d886046a3018128b7
Michael Lo <nz.ampersand@gmail.com>
2013-08-02 15:33:31 +1245

    Added more tests, still compiles. Find out why only two tests executing.

--- TestMoney.java
+++ TestMoney.java
@@ -3,43 +3,65 @@ package se254.money;
  * SOFTENG 254 2011 Assignment 1 submission
  *
  * Author: (Michael Lo, mlo450)
+ *
+ * GitHub: https://github.com/Ampersandnz/254a01.git
  **/
 
 import junit.framework.TestCase;
 
 public class TestMoney extends TestCase {
-	private Money money;
-
+https://github.com/Ampersandnz/254a01.git
     public void setUp() {
-    	money = null;
     }
 
     public void tearDown() {
-    	money = null;
     }
 
-    //Ensure that the default constructor, Money(), creates a Money with an amount of $0.00
+    //Ensure that the default constructor, Money(), creates a Money with an amount of $0.00.
     public void testZeroConstructor() {
-    	money = new Money();
+    	Money money = new Money();
     	assertSame("$0.00", money.toString());
-		System.out.println(money.toString());
     }
 
-    public void testConstructorWithZero() {
-    	money = new Money();
+    //Ensure that the constructor Money(int dollars, int cents) correctly creates a Money with an amount of $0.00.
+    public void testConstructorAWithZero() {
+    	Money money = new Money(0,0);
     	assertEquals("$0.00", money.toString());
     }
 
+    //Ensure that the constructor Money(int dollars, int cents) correctly creates a Money with a negative amount of dollars, cents, and both.
+    public void testConstructorAWithNegativeAmounts() {
+    	Money money = new Money(-3,0);	
+    	Money money = new Money(0,-15);   
+    	Money money = new Money(-3,15);
+    	assertEquals("-$3.00", money.toString());    
+    	assertEquals("-$0.15", money.toString()); 	
+    	assertEquals("-$3.15", money.toString());    	
+    }
 
+    //Ensure that the constructor Money(int dollars, int cents) correctly creates a Money with a positive amount of dollars, cents, and both.
+    public void testConstructorAWithPositiveAmounts() {
+    	Money money2 = new Money(3,0);
+    	Money money1 = new Money(0,15);
+    	Money money3 = new Money(3,15);
+    	assertEquals("$3.00", money2.toString());
+    	assertEquals("$0.15", money1.toString());
+    	assertEquals("$3.15", money3.toString());
+    }
 
-
-
-
-
-
-
-
-
+    /*
+    //Ensure that the constructor Money(int dollars, int cents) correctly handles an input of >99 cents.
+    public void testConstructorAWithMoreThan99Cents() {
+    	Money money = new Money(0,150);
+    	##Throws exception?
+    } */
+    
+    //Ensure that the constructor Money(int dollars, int cents) correctly creates a Money with a positive amount of both dollars and cents.
+    public void testImmutability() {
+    	Money money = new Money(3,15);
+    	money = new Money(3,14);
+    	assertEquals("$3.15", money.toString());
+    }
 
     /**
      * DO NOT DELETE THIS
